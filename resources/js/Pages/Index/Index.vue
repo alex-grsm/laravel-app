eb2f59df898bb74882190e51430a60d8a6bf2e03
Алексей <ejuk@mac.station>
2024-08-26 12:28:10 +0100

    first commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>HELLO!</h1>
+    <Link href="/hello"
+    >Show page</Link>
+    <div>The message is {{ message }}</div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    message: String,
+});
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Show</h1>
+    <Link href="/">Home page</Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+// import MainLayout from "@/Layouts/MainLayout.vue";
+// defineOptions({ layout: MainLayout });
+</script>

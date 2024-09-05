393907480c2d5ae001d4ad071215f7578e566c01
Алексей <ejuk@MAC.station>
2024-09-05 19:44:24 +0100

    Create listing form

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,6 +2,7 @@
     <Link href="/">Home page</Link>&nbsp;
     <Link href="/hello">Show page</Link>&nbsp;
     <Link href="/listing">Listing page</Link>&nbsp;
+    <Link href="/listing/create">Create page</Link>&nbsp;
     <!-- <div>The page with time {{ timer }}</div> -->
     <br /><br />
 
@@ -10,7 +11,7 @@
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
-import { ref } from "vue";
+// import { ref } from "vue";
 
 // const timer = ref(0);
 // setInterval(() => timer.value++, 1000);

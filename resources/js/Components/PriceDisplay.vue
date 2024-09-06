4a32d948eabe4633521ebeab85d0bb738863ef02
Алексей <ejuk@mac.station>
2024-09-06 20:45:37 +0100

    Listings styles

---
+++ resources/js/Components/PriceDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+    <span>{{ formattedPrice }}</span>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    price: [Number, String],
+    locale: { type: String, default: "en-US" },
+    currency: { type: String, default: "USD" },
+});
+
+const formattedPrice = computed(() => {
+    const priceNumber = Number(props.price);
+
+    if (isNaN(priceNumber)) {
+        return "N/A";
+    }
+    return priceNumber.toLocaleString(props.locale, {
+        style: "currency",
+        currency: props.currency,
+        maximumFractionDigits: 0,
+    });
+});
+</script>

622ccd165d7a0fe74d37349b5465b02d67dd5dbd
Алексей <ejuk@mac.station>
2024-09-06 12:24:30 +0100

    Computed data, Displaying flash msg

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,6 +3,10 @@
     <Link href="/hello">Show page</Link>&nbsp;
     <Link href="/listing">Listing page</Link>&nbsp;
     <Link href="/listing/create">Create page</Link>&nbsp;
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+
     <!-- <div>The page with time {{ timer }}</div> -->
     <br /><br />
 
@@ -10,9 +14,22 @@
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
-// import { ref } from "vue";
+import { computed } from 'vue';
+import { Link, usePage } from "@inertiajs/vue3";
 
+const page = usePage();
+const flashSuccess = computed(
+  () => page.props.flash.success,
+);
+
+// import { ref } from "vue";
 // const timer = ref(0);
 // setInterval(() => timer.value++, 1000);
 </script>
+
+<style scoped>
+  .success {
+    background-color: green;
+    color: white;
+  }
+</style>

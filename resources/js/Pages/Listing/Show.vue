22cea5511e2134639cb74b38107c7d38d164bc1f
Алексей <ejuk@mac.station>
2024-09-07 21:08:54 +0100

    Payment Logic

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -19,8 +19,36 @@
             </Box>
 
             <Box>
-                <template #header> Offer </template>
-                Make an offer
+                <template #header> Monthly Payment </template>
+                <div>
+                    <label class="label">Interest rate ({{ interestRate }}%)</label>
+                    <input
+                        v-model.number="interestRate"
+                        type="range"
+                        min="0.1"
+                        max="30"
+                        step="0.1"
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                    />
+
+                    <label class="label">Duration ({{ duration }} years)</label>
+                    <input
+                        v-model.number="duration"
+                        type="range"
+                        min="3"
+                        max="35"
+                        step="1"
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                    />
+
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">Your monthly payment</div>
+                        <PriceDisplay
+                            :price="monthlyPayment"
+                            class="text-3xl"
+                        />
+                    </div>
+                </div>
             </Box>
         </div>
     </div>
@@ -30,9 +58,18 @@
 import ListingAddress from "@/Components/ListingAddress.vue";
 import ListingSpace from "@/Components/ListingSpace.vue";
 import PriceDisplay from "@/Components/PriceDisplay.vue";
-import Box from "../../Components/UI/Box.vue";
+import Box from "@/Components/UI/Box.vue";
 
-defineProps({
+import { ref } from "vue";
+import {useMonthlyPayment} from '@/Composables/useMonthlyPayment'
+
+const props = defineProps({
     listing: Object,
 });
+
+const interestRate = ref(2.5);
+const duration = ref(25);
+
+// Calculate monthly payment
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration);
 </script>

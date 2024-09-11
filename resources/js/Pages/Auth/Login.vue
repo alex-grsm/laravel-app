269c313d7ad56f1a3514c2879aa412f363700ecf
Алексей <ejuk@MAC.station>
2024-09-11 18:26:57 +0100

    Authentication Process

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,27 +1,40 @@
 <template>
     <form @submit.prevent="login">
-      <div class="w-1/2 mx-auto">
-        <div>
-          <label for="email" class="label">E-mail (username)</label>
-          <input id="email" v-model="form.email" type="text" class="input" />
-          <div class="input-error">Potential errors</div>
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label for="email" class="label">E-mail (username)</label>
+                <input
+                    id="email"
+                    v-model="form.email"
+                    type="text"
+                    class="input"
+                />
+                <InputError :error-message="form.errors.email" />
+            </div>
+            <div class="mt-4">
+                <label for="password" class="label">Password</label>
+                <input
+                    id="password"
+                    v-model="form.password"
+                    type="password"
+                    class="input"
+                />
+                <InputError :error-message="form.errors.password" />
+            </div>
+            <div class="mt-4">
+                <button class="btn-primary w-full" type="submit">Login</button>
+            </div>
         </div>
-        <div class="mt-4">
-          <label for="password" class="label">Password</label>
-          <input id="password" v-model="form.password" type="password" class="input" />
-        </div>
-        <div class="mt-4">
-          <button class="btn-primary w-full" type="submit">Login</button>
-        </div>
-      </div>
     </form>
-  </template>
+</template>
 
-  <script setup>
-  import { useForm } from '@inertiajs/vue3'
-  const form = useForm({
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+import InputError from "@/Components/InputError.vue";
+
+const form = useForm({
     email: null,
     password: null,
-  })
-  const login = () => form.post(route('login.store'))
-  </script>
+});
+const login = () => form.post(route("login.store"));
+</script>

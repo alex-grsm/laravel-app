75d072c016fab21619689e6967a04aa1d5a0d7a9
Алексей <ejuk@mac.station>
2024-09-12 19:02:52 +0100

    User Registration Form

---
+++ resources/js/Pages/UserAccount/Create.vue
@@ -0,0 +1,62 @@
+<template>
+    <form @submit.prevent="register">
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label for="name" class="label">Your Name</label>
+                <input
+                    id="name"
+                    v-model="form.name"
+                    type="text"
+                    class="input"
+                />
+                <InputError :error-message="form.errors.name" />
+            </div>
+            <div class="mt-4">
+                <label for="email" class="label">E-mail</label>
+                <input
+                    id="email"
+                    v-model="form.email"
+                    type="text"
+                    class="input"
+                />
+                <InputError :error-message="form.errors.email" />
+            </div>
+            <div class="mt-4">
+                <label for="password" class="label">Password</label>
+                <input
+                    id="password"
+                    v-model="form.password"
+                    type="password"
+                    class="input"
+                />
+                <InputError :error-message="form.errors.password" />
+            </div>
+            <div class="mt-4">
+                <label for="password_confirmation" class="label">Confirm password</label>
+                <input
+                    id="password_confirmation"
+                    v-model="form.password_confirmation"
+                    type="password"
+                    class="input"
+                />
+                <!-- <InputError :error-message="form.errors.password_confirmation" /> -->
+            </div>
+            <div class="mt-4">
+                <button class="btn-primary w-full" type="submit">Create Account</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+import InputError from "@/Components/InputError.vue";
+
+const form = useForm({
+    name: null,
+    email: null,
+    password: null,
+    password_confirmation: null,
+});
+const register = () => form.post(route(""));
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
-
+use App\Http\Controllers\UserAccountController;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show'])
@@ -22,3 +22,6 @@ Route::post('login', [AuthController::class, 'store'])
     ->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])
     ->name('logout');
+
+Route::resource('user-account', UserAccountController::class)
+    ->only(['create']);

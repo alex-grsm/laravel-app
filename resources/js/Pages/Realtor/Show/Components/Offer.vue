9c3b01c9580244b46445539a571738326debf32c
alex-grsm <alexey.grsm@gmail.com>
2024-10-03 18:13:38 +0300

    UI Improvements

--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -33,7 +33,7 @@
             </div>
             <div>
                 <Link
-                    v-if="notSold"
+                    v-if="!isSold"
                     :href="route('realtor.offer.accept', { offer: offer.id })"
                     method="put"
                     class="btn-outline text-xs font-medium"
@@ -55,12 +55,10 @@ import { computed } from "vue";
 const props = defineProps({
     offer: Object,
     listingPrice: Number,
+    isSold: Boolean,
 });
 
 const difference = computed(() => props.offer.amount - props.listingPrice);
 const madeOn = computed(() => new Date(props.offer.created_at).toDateString());
 
-const notSold = computed(
-    () => !props.offer.accepted_at && !props.offer.rejected_at
-);
 </script>
